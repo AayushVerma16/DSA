bf78ef8dd77701b2c3f9a462cc2f4d22d3e8484d
Aayush Verma <aayushverma1602@gmail.com>
2026-07-12 13:21:56 +0530

    Time: 2 ms (99.69%) | Memory: 45.4 MB (10.47%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -3,7 +3,7 @@ class Solution {
     public void findSubsets(int ind, int[] nums, List<Integer> ds, List<List<Integer>> ansList) {
         ansList.add(new ArrayList<>(ds));
         for (int i = ind; i < nums.length; i++) {
-            if (i != ind && nums[i] == nums[i - 1]) {
+            if (i > ind && nums[i] == nums[i - 1]) {
                 continue;
             }
             ds.add(nums[i]);
